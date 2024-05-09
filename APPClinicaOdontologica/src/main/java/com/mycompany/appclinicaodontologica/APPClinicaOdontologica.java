dfaa8ef34bba972d959db76d5a311402c6218fa0
luiz6 <luiz6@TDZ-PC>
2024-05-09 14:05:23 -0400

    Primeiro Commit

---
+++ APPClinicaOdontologica/src/main/java/com/mycompany/appclinicaodontologica/APPClinicaOdontologica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.appclinicaodontologica;
+
+/**
+ *
+ * @author luiz6
+ */
+public class APPClinicaOdontologica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
